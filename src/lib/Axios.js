b1a60ece3f3ee93dcead8a378dc4fbdeb10aad80
Sarah Lopez <slopez323@gmail.com>
2022-12-07 11:46:18 -0500

    backend port 4000

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:3001/api' : '/api',
-    timeout: 50000
-})
+  baseURL:
+    process.env.REACT_APP_AXIOS === "development"
+      ? "http://localhost:4000/api"
+      : "/api",
+  timeout: 50000,
+});
 
 export default Axios;
 
-
-// for above Axios.js you need 
+// for above Axios.js you need
 // either .env or package.json below
 //
 // .env
@@ -20,8 +22,8 @@ export default Axios;
 // "dev": "REACT_APP_AXIOS='development' react-scripts start"
 //
 // ------ OR ---------
-// 
-// baseURL: process.env.REACT_APP_AXIOS 
+//
+// baseURL: process.env.REACT_APP_AXIOS
 //
 // package.json
 // "start": "REACT_APP_AXIOS=/api PORT=3000 react-scripts start",
